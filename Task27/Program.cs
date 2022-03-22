a74bb246f238292d30eebd267e45938ad4b09bc5
Yaroslav63 <era6300@mail.ru>
2022-03-23 02:01:17 +0300

    Add Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+while (number > 0)
+{
+    s = s + number % 10;
+    number = number /10;
+}
+Console.WriteLine(s);
