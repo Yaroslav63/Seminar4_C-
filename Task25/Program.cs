2c53b7ba35f8294183e746b5cde6478f1865c83d
Yaroslav63 <era6300@mail.ru>
2022-03-23 01:12:22 +0300

    Add Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int a = number1;
+int b = number2;
+System.Console.WriteLine($"{a}^{b} = " + Math.Pow(a, b));
